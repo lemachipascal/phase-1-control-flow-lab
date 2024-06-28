dea9d259d419009d66689d49f27638e92f9bdc46
Lemachipascal-dev <lemachijo421@gmail.com>
2024-06-28 14:27:14 +0300

    first lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Distance){
+if(Distance<= 400){
+  return 'This one is on me!'
+}else if(Distance > 400 && Distance < 2000){
+  return 'That will be twenty bucks.'
+}else if(Distance >= 2000 && Distance <= 2500){
+  return 'I will gladly take your thirty bucks.'
+}else if(Distance > 2500){
+  return 'No can do.'
+}
+}
+Distance(2501)
+
+
+function ternaryCheckCity(city){
+  if(city === 'NYC'){
+    return "Ok, sounds good."
+  }else {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === 'generous'){
+    return "Thank you so much."
+  }else if(tip === 'not as generous'){
+    return 'Thank you.'
+  }else{
+    return 'Bye.'
+  }
 }
